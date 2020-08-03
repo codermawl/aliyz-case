1c943ae7866593d730d1d2abb77cafb96748e7b1
mawl <mawenlei@tusdao.com>
2020-08-03 15:08:35 +0800

    mdf aliyz case

---
+++ integration/demo-web/src/main/java/com/aliyz/demoweb/DemoApplication.java
@@ -0,0 +1,36 @@
+package com.aliyz.demoweb;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * All rights Reserved, Designed By www.tusdao.com
+ *
+ * <p></p>
+ * Created by mawl at 2020-08-03 14:26
+ * Copyright: 2020 www.tusdao.com Inc. All rights reserved.
+ */
+@SpringBootApplication
+@EnableScheduling
+@MapperScan(basePackages = {"com.tusdao.**.mapper"})
+public class DemoApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    /**
+     * 添加tomcat服务器中运行支持
+     * visit http://localhost:8080/demo-web/api/monitor/alive
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
+    }
+}
